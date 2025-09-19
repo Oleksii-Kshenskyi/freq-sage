b077185b17e4cd2646cd1e1968a1645b1875c673
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2025-09-19 20:07:49 +0300

    Empty C# dummy project.

--- migrations/2025-07-09-005823_create_sentence_rankings/up.sql
+++
@@ -1,7 +0,0 @@
--- Your SQL goes here
-CREATE TABLE IF NOT EXISTS sentence_rankings (
-  id          INTEGER PRIMARY KEY AUTOINCREMENT,
-  sentence    TEXT NOT NULL,
-  ranking     BIGINT NOT NULL,
-  lang        TEXT NOT NULL
-);
--- migrations/2025-09-17-184948_add_unique_constraint_sentence_rankings/down.sql
+++
@@ -1,3 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP INDEX IF EXISTS idx_sentence_rankings_unique;
-DROP INDEX IF EXISTS idx_frequencies_unique;
